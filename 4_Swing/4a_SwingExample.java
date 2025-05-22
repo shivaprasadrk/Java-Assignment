8365425e00c82ea31719a9d38cabdd20a4fa2623
shivaprasadrk <152306638+shivaprasadrk@users.noreply.github.com>
2025-05-22 18:27:21 +0530

    Add files via upload

---
+++ 4_Swing/4a_SwingExample.java
@@ -0,0 +1,42 @@
+// 4a. Basic hello program of Swing displaying the message "Hello! VI C , Welcome to Swing Programming"
+// in Blue color plain font with font size of 32 using JFrame and JLabel
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.*;
+
+// A simple Swing application.
+class SwingExample {
+    SwingExample() {
+        // Create a new JFrame container.
+        JFrame jfrm = new JFrame("A Simple Swing Application");
+
+        // Give the frame an initial size.
+        jfrm.setSize(700, 150); // Increased size for better message visibility
+
+        // Terminate the program when the user closes the application.
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a text-based label.
+        JLabel jlab = new JLabel(" Hello! VI C , Welcome to Swing Programming!");
+
+        // Set properties for label: font style, size and color
+        jlab.setFont(new Font("Verdana", Font.PLAIN, 32));
+        jlab.setForeground(new Color(0, 0, 255)); // Blue color
+
+        // Add the label to the content pane.
+        jfrm.add(jlab);
+
+        // Display the frame.
+        jfrm.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        // Create the frame on the event dispatching thread.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new SwingExample();
+            }
+        });
+    }
+}
